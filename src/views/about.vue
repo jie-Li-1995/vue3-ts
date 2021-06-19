9be701a92ea96ab586f0b7e3a7290eddc68b1a27
jie-Li-1995 <2893535402@qq.com>
2021-06-19 23:20:29 +0800

    ::marker
    marker

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,5 +1,53 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <ul>
+      <li>1111</li>
+      <li>1111</li>
+      <li>1111</li>
+      <li>1111</li>
+      <li>1111</li>
+    </ul>
+    <ol>
+      <li>1111</li>
+      <li>1111</li>
+      <li>1111</li>
+      <li>1111</li>
+      <li>1111</li>
+    </ol>
+    <ol>
+      <li>1111</li>
+      <li>1111</li>
+      <li>1111</li>
+      <li>1111</li>
+      <li>1111</li>
+    </ol>
   </div>
 </template>
+<style lang="less">
+.about {
+  counter-reset: subsection;
+  h1:before {
+    counter-increment: subsection;
+    content: counter(subsection) "-----";
+  }
+  ul {
+    li {
+      &::marker {
+        content: "-----";
+      }
+    }
+  }
+  ol {
+    li::marker {
+      content: counter() "-----";
+    }
+  }
+}
+</style>
